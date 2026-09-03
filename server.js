6c45f17c1f0c4fff62aac3dfc11ecabcebc65254
Kunbher-Sahab <abdulmoiaz97@gmail.com>
2026-09-03 16:40:07 -0700

    Change PUBLIC directory to root path

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const crypto = require("crypto");
 const { URL } = require("url");
 
 const ROOT = __dirname;
-const PUBLIC = path.join(ROOT, "public");
+const PUBLIC = ROOT;
 const DATA_DIR = path.join(ROOT, "data");
 const DB_FILE = path.join(DATA_DIR, "db.json");
 fs.mkdirSync(DATA_DIR, { recursive: true });
